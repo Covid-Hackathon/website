b6a2bc405c7371f3b1ba232e409b50bfa0446b3e
Ruben Correoso <rubencorreoso@ctnm.local>
2020-09-15 17:06:39 +0200

    add header and bootstrap

---
+++ app/src/components/Header.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <header
+        className="site-navbar py-4 bg-white js-sticky-header site-navbar-target"
+        role="banner"
+      >
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-11 col-xl-2">
+              <h1 className="mb-0 site-logo">
+                <a href="index.html" className="text-black h2 mb-0">
+                  Create<span className="text-primary">.</span>{" "}
+                </a>
+              </h1>
+            </div>
+            <div className="col-12 col-md-10 d-none d-xl-block">
+              <nav
+                className="site-navigation position-relative text-right"
+                role="navigation"
+              >
+                <ul className="site-menu main-menu js-clone-nav mr-auto d-none d-lg-block">
+                  <li>
+                    <a href="#home-section" className="nav-link">
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#work-section" className="nav-link">
+                      Work
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#services-section" className="nav-link">
+                      Services
+                    </a>
+                  </li>
+                  <li className="has-children">
+                    <a href="#about-section" className="nav-link">
+                      About
+                    </a>
+                    <ul className="dropdown">
+                      <li>
+                        <a href="#about-section">Specialties</a>
+                      </li>
+                      <li>
+                        <a href="#team-section">Our Team</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#blog-section" className="nav-link">
+                      Blog
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#contact-section" className="nav-link">
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+
+            <div
+              className="d-inline-block d-xl-none ml-md-0 mr-auto py-3"
+              style={{position: 'relative', top: '3px'}}
+            >
+              <a href="#" className="site-menu-toggle js-menu-toggle text-black">
+                <span className="icon-menu h3"></span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/css/index.css';
 import App from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+/*import '../node_modules/bootstrap/dist/js/bootstrap.min.js';*/
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
